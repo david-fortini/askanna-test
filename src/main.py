375229873f204ade97a3056f4983152bcfe8f413
david-fortini <fortini.david@gmail.com>
2021-01-30 19:22:46 +0100

    added data payload

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import uuid
+import os
 
 
 def say_hello() -> None:
@@ -12,6 +13,8 @@ if __name__ == "__main__":
     policy = json.dumps(describe)
     s3 = boto3.client('s3')
     uuid_key = str(uuid.uuid4())
-    s3.put_object(Body=policy,
-                  Bucket='voldemort-test',
-                  Key='ask-anna-uploaded' + uuid_key)
+    _ = s3.put_object(Body=policy,
+                      Bucket='voldemort-test',
+                      Key='ask-anna-uploaded' + uuid_key)
+    var_job = os.getenv("example")
+    print(f'var job is = {var_job}')
