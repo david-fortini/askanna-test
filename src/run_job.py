b3dd8d3308256835ebab77cfc455d94ce9a1bd56
david-fortini <fortini.david@gmail.com>
2021-01-30 19:25:01 +0100

    added job

--- src/run_job.py
+++ src/run_job.py
@@ -1,7 +1,8 @@
 from askanna import run
 
 """
-askanna.core.exceptions.PostError: 404 - Something went wrong while starting a run: Not Found
+askanna.core.exceptions.PostError: 404 -
+Something went wrong while starting a run: Not Found
 """
 
 json_data = {"example": "payload-python"}
