15181599ac887da7b39736de7886fc04a4870cf4
david-fortini <fortini.david@gmail.com>
2021-01-30 16:40:42 +0100

    added uiid key to s3 upload

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
+import boto3
+import json
+import uuid
+
+
 def say_hello() -> None:
     print('Hello askanna!')
+
+
+if __name__ == "__main__":
+    describe = {'some file': 'some string content'}
+    policy = json.dumps(describe)
+    s3 = boto3.client('s3')
+    uuid_key = str(uuid.uuid4())
+    s3.put_object(Body=policy,
+                  Bucket='voldemort-test',
+                  Key='ask-anna-uploaded' + uuid_key)
